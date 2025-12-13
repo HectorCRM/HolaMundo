7ec86263bc78b13060746b8a0d55802cb4ce9f54
HectorCRM <monroygti2.0@gmail.com>
2025-12-13 06:30:41 +0100

    Corrección código c#

--- Lenguajes/C#/src/Hola.cs
+++ Lenguajes/C#/src/Hola.cs
@@ -1,8 +1,4 @@
-/*Importa el namespace System que contiene la clase necesaria para imprimir por pantalla,
-podria omitirse y habria que escribir System.Console.WriteLine en lugar de Console.WriteLine
-*/
 using System;
-//La estructura es igual a la de Java, solo cambia System.out.println por Console.WriteLine
 public class Hola {
 	public static void Main(string[] args) {
 		Console.WriteLine("Hola mundo!");
